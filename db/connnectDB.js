dd56038264a01d76e0b8725181d777760ad67ddf
hetchawda007 <hetchawda44@gmail.com>
2025-02-28 09:41:45 +0530

    Fixed mongo conection bug.

--- db/connnectDB.js
+++ db/connnectDB.js
@@ -4,14 +4,11 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     console.log('Attempting MongoDB connection...');
-    await mongoose.connect(`${process.env.MONGO_URL}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true, // Add this for more stable connections
-    });
+    await mongoose.connect(`${process.env.MONGO_URL}`);
     console.log('MongoDB Connected:');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-    throw new Error('Database connection failed'); // Helps debug in logs
+    throw new Error('Database connection failed');
   }
 };
 
