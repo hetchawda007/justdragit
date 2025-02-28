b15d9c6d944f5a1417f5d664a3118fa63c0ebd71
hetchawda007 <hetchawda44@gmail.com>
2025-02-28 09:32:29 +0530

    Fixed login page bug

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ import { signIn } from 'next-auth/react'
 import { getSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
 
-const page = () => {
+const Page = () => {
 
     useEffect(() => {
         const func = async () => {
@@ -55,4 +55,4 @@ const page = () => {
     )
 }
 
-export default page
+export default Page
